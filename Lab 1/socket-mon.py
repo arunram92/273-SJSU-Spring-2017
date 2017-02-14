9d0c3a36acd95f7362f08d364f42875e61f03ca9
Y.Arun Ramanathan <y.arunram@gmail.com>
2017-02-14 08:30:35 +0530

    Cmpe 273
    Sort in desc order
    Added commets

--- Lab 1/socket-mon.py
+++ Lab 1/socket-mon.py
@@ -1,8 +1,13 @@
 import psutil
 
+#dictionary for storing the PIDs and respective number of socket connections
 numberOfConnections = {}
 connectionDetails = []
 
+"""
+Loop through all the tcp connections.
+Store connection details and the number of connections for each process.
+"""
 for connection in psutil.net_connections("tcp"):
         if connection.raddr and connection.laddr:
             raddr =  "%s@%s" % (connection.raddr)
@@ -10,9 +15,11 @@ for connection in psutil.net_connections("tcp"):
             connectionDetails.append({"pid":connection.pid,"laddr":laddr,"raddr":raddr,"status":connection.status})
             numberOfConnections[connection.pid] = numberOfConnections[connection.pid]+1 if connection.pid in numberOfConnections else 1
 
+#sort the numberOfConnections dict in desc order
 connectionsSortedByCount = [(value,key) for key,value in numberOfConnections.items()]
-connectionsSortedByCount.sort()
+connectionsSortedByCount.sort(reverse=True)
 
+#Print the connection details based on the sorted list of PIDs
 print '"pid","laddr","raddr","status"'
 for value,key in connectionsSortedByCount:
     for connection in connectionDetails:
